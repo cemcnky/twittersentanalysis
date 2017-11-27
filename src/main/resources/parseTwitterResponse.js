81f326247100e214369c9c92db783ba3911d72a2
cemcnky <000000a>
2017-11-27 13:16:57 +0100

    script

--- src/main/resources/parseTwitterResponse.js
+++ src/main/resources/parseTwitterResponse.js
@@ -1,6 +1,32 @@
 // fetch execution variables
 var response = connector.getVariable("response");
 
-print(response);
+var json = S(response); //camunda spin, parsing json
+
+var elements = json.elements(); // each element is a tweet block (not text)
+
+print(json);
+
+var tweets = [];
+
+for (var i = 0; i < elements.length; i++) {
+
+    var tweetTextProperty = elements.get(i).prop("tweetText"); //tweetText property of the element
+
+    tweets.push(tweetTextProperty);
+
+    //print(tweetTextProperty);
+}
+
+payload = encodeURIComponent("tweet=");
+
+for (index = 0; index < tweets.length; ++index) {
+    payload += encodeURIComponent(tweets[index]) + '&';
+}
+
+
+print(payload);
+
+
 // use camunda-spin jsonPath to test if date is a holiday
 //!holidays.jsonPath(query).elementList().isEmpty();
